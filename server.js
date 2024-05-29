3c1dce461af03f0b7d622a8ed9506fdfcf0240c0
Abhisek Phago limbu <112129568+AbhisekLimbu@users.noreply.github.com>
2024-05-29 16:43:01 -0500

    server is serving the frontedn

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const connectDB = require('./database');
 const postingDB = require('./routes/postingDB');
 const storyRoutes = require('./routes/storyRoutes'); // Corrected import path
-const deleteRoutes = require('./routes/delete');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
@@ -23,15 +22,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/uploads', express.static('uploads'));
 
-app.get('/', (req, res) => {
-  console.log("Hello World");
-  res.send("Hello World");
-});
+const buildPath = path.join(__dirname, './client/build');
+app.use(express.static(buildPath));
 
+// Add your API routes
 app.use('/localstory', postingDB);
 app.use('/story', storyRoutes); // Use the correct path for story routes
-app.use('/story', deleteRoutes);
+
+// All other GET requests not handled before will return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(buildPath, 'index.html'));
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log('Server is running on port', port);
